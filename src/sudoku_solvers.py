721fb8555a05b90ce2983e38716cac5671d68f5b
PrimRGB <primrgb@gmail.com>
2020-09-20 15:28:26 +0300

    Added gitignore

--- src/sudoku_solvers.py
+++ src/sudoku_solvers.py
@@ -4,3 +4,18 @@ def basic_assign(cell: Cell):
     if sum(cell.candidates) == 1:
         cell.assign_value(cell.candidates.index(1))
 
+
+def reverse_assign(cell: Cell):
+    pass
+
+
+def basic_eliminate(cell: Cell):
+    pass
+
+
+def group_eliminate(cell: Cell):
+    pass
+
+
+def structure_intersection_eliminate(cell: Cell):
+    pass
